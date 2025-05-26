d522aa88d1c145927cea31f8ab5b33e1226e59b5
Caracol <rafaelfariasm@live.com>
2025-05-26 17:05:12 -0300

    fixed ghpages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/tankinho/",
+
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
